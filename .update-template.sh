7daffc691ee0250a4988056b2ed1b4d9670c5857
OpenGG <liy099@gmail.com>
2021-12-27 14:59:01 +0800

    update script

--- .update-template.sh
+++ .update-template.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
-curl https://gist.githubusercontent.com/OpenGG/258b0eb9a68a59054cdb7bbed3bee448/raw/82f2df57c3b940c36dad2aaa789e5ae1b8009836/update-template.sh -o /var/tmp/codesandbox-update-template.sh
-source /var/tmp/codesandbox-update-template.sh
-rm /var/tmp/codesandbox-update-template.sh
+
+scriptUrl=https://gist.githubusercontent.com/OpenGG/258b0eb9a68a59054cdb7bbed3bee448/raw/update-template.sh
+scriptFile=/var/tmp/codesandbox-update-template.sh
+
+curl $scriptUrl -o $scriptFile
+source $scriptFile
+rm $scriptFile
