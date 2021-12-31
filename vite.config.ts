47da8afe154f508051e273c1319b6af5069c3bbd
Github CI <ci@github.com>
2021-12-31 12:16:24 +0000

    bump

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: { hmr: { port: 443 } },
+export default {
+  server: process.cwd() === "/sandbox" defineConfig({defineConfig({ {
+    hmr: {
+      port: 443
+    }
+  },
   plugins: [vue()]
 })
